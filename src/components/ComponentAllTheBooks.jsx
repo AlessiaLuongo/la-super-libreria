1fcd50b7fa074e1b9cf9d4a363e33825289a63ac
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-01-31 16:59:47 +0100

    button

--- src/components/ComponentAllTheBooks.jsx
+++ src/components/ComponentAllTheBooks.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import { Component } from "react";
 import SingleBook from "./SingleBook";
@@ -14,33 +15,46 @@ class ComponentAllTheBooks extends Component {
   render() {
     return (
       <Container>
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault();
-            this.setState({
-              ...this.state,
-              searchResult: this.props.libri.filter((libro) => {
-                return libro.title
-                  .toLowerCase()
-                  .includes(this.state.inputValue.toLowerCase());
-              }),
-            });
-          }}
-        >
-          <Form.Control
-            type="text"
-            placeholder="Search"
-            className=" my-5"
-            value={this.state.inputValue}
-            onChange={(e) => {
+        <Row className="justify-content-center">
+          <Form
+            className="my-3 d-flex justify-content-end"
+            onSubmit={(e) => {
+              e.preventDefault();
               this.setState({
                 ...this.state,
-                inputValue: e.target.value,
+                searchResult: this.props.libri.filter((libro) => {
+                  return libro.title
+                    .toLowerCase()
+                    .includes(this.state.inputValue.toLowerCase());
+                }),
               });
             }}
-          />
-          <Button type="submit">Cerca</Button>
-        </Form>
+          >
+            <Col
+              xs={12}
+              sm={10}
+              md={6}
+              lg={3}
+              className=" d-flex justify-content-center"
+            >
+              <Form.Control
+                type="text"
+                placeholder="Search"
+                value={this.state.inputValue}
+                onChange={(e) => {
+                  this.setState({
+                    ...this.state,
+                    inputValue: e.target.value,
+                  });
+                }}
+              />
+
+              <Col className="text-center">
+                <Button type="submit">Cerca</Button>
+              </Col>
+            </Col>
+          </Form>
+        </Row>
 
         <Row className="g-3">
           {this.state.inputValue && this.state.searchResult.length > 0
